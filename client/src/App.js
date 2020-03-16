1902ba5f2a16cbb0a881f0c80bcbbfe52b25b22e
moshecstern@gmail.com <moshecstern@gmail.com>
2020-03-16 11:25:23 -0400

    home/ material ui added

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import {BrowserRouter} from "react-router-dom";
 import {Switch, Route} from "react-router-dom";
 import Login from "./components/login/login"
 import Signup from "./components/signup/signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/" component={Home}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/books" component={Books} />
+        <Route exact path="/signin" component={Login}/>
       </Switch>
     </BrowserRouter>
   );
